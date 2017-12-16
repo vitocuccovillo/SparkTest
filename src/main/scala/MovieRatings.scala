cb8fe6a8b137618cbd509ba43612f881e0029a5e
vitocuccovillo <vito.cuccovillo@gmail.com>
2017-12-16 17:22:38 +0100

    fixed movie type count

--- src/main/scala/MovieRatings.scala
+++ src/main/scala/MovieRatings.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object MovieRatings {
@@ -7,11 +8,21 @@ object MovieRatings {
     val conf = new SparkConf().setMaster("local[*]").setAppName("myApp")
     val sc = new SparkContext(conf)
 
-    var moviesDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/moviesLight.csv")
-                     .map(movie => (Array(movie.split(",").takeRight(1)(0).split('|')).toVector,Array[String](movie.split(",")(0))))
+    val moviesDatasetRDD = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/moviesLight.csv")
+//    val movies: RDD[(String, String)] = moviesDatasetRDD.map(x => (x.split(",")(0), x.split(",")(x.split(",").length - 1)))
+//    movies.foreach(println)
+
+    val genereWithMovies = moviesDatasetRDD
+                          .map(movie => (movie.substring(movie.lastIndexOf(',')+1,movie.length).split('|')(0),Array(movie.substring(0,movie.indexOf(',')))))
+                          .reduceByKey(_++_)
+                          .foreach(println)
+
+   /* var moviesDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/moviesLight.csv")
+                     //.map(movie => (Array(movie.substring(movie.lastIndexOf(','),movie.length-1).split('|').distinct).toVector,Array(movie.split(",")(0).toVector)))
+                     .map(movie => (movie.substring(movie.lastIndexOf(','),movie.length-1).split('|'),Array(movie.substring(0,movie.indexOf(',')))))
                      .reduceByKey(_++_)
                      .collect()
-                     .foreach(println)
+                     .foreach(println)*/
 
 //    var ratingsDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/ratings.csv")
 //                      .flatMap(movie => movie.split(",").takeRight(1)(0).split('|'))
