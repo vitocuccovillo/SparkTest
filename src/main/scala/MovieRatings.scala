9d6e55dd3b60b2e6e70b084e7add982b495155f1
vitocuccovillo <vito.cuccovillo@gmail.com>
2017-12-16 12:00:07 +0100

    fixed movie type count

--- src/main/scala/MovieRatings.scala
+++ src/main/scala/MovieRatings.scala
@@ -8,11 +8,17 @@ object MovieRatings {
     val sc = new SparkContext(conf)
 
     var moviesDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/moviesLight.csv")
-                     .flatMap(movie => movie.split(",").takeRight(1)(0).split('|'))
-                     .map((_,1))
-                     .reduceByKey(_+_)
-                     .collect.toMap
+                     .map(movie => (Array(movie.split(",").takeRight(1)(0).split('|')).toVector,Array[String](movie.split(",")(0))))
+                     .reduceByKey(_++_)
+                     .collect()
                      .foreach(println)
+
+//    var ratingsDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/ratings.csv")
+//                      .flatMap(movie => movie.split(",").takeRight(1)(0).split('|'))
+//                      .map((_,1))
+//                      .reduceByKey(_+_)
+//                      .collect.toMap
+//                      .foreach(println)
 
   }
 
