b8de275e306a6bcb79bcf7d91c0e4951fd336dd1
vitocuccovillo <vito.cuccovillo@gmail.com>
2017-12-18 16:16:44 +0100

    exercise completed!

--- src/main/scala/MovieRatingsOptimized.scala
+++ src/main/scala/MovieRatingsOptimized.scala
@@ -13,37 +13,9 @@ object MovieRatingsOptimized {
     val moviesWithGen = moviesDatasetRDD.map(movie => (movie.substring(0,movie.indexOf(',')),movie.substring(movie.lastIndexOf(',')+1,movie.length)))
     val ratings = ratingsDatasetRDD.map(rating => (rating.split(",")(1),rating.split(",")(2)))
     val merged = moviesWithGen.join(ratings)
-    //val bygen = merged.flatMap{ case(f,(r,g)) => (g,r)}
-    // QUESTO MANIPOLA LE COPPIE! da coppia di coppie a coppia singola
-    //val rdd = merged.map{ case (film, (gen,rat)) => (gen,rat) }.reduceByKey(_+_).foreach(println)
-
-    //val rd = merged.flatMap(x => x._2._1.split('|').map(y => (y,(x._2._2.toDouble,1)))).reduceByKey{case ((r1,c1),(r2,c2)) => ((r1*c1 + r2*c2)/(c1+c2), c1 + c2)}
-    //val rd = merged.flatMap(x => x._2._1.split('|').map(y => (y,(x._2._2.toDouble,1)))).reduceByKey{case ((r1,c1),(r2,c2)) => ((r1*c1 + r2*c2)/(c1+c2), c1 + c2)}
     val rd = merged.flatMap(x => x._2._1.split('|').map(y => (y,(x._2._2.toDouble,1)))).reduceByKey((r1,r2) => ((r1._1*r1._2 + r2._1*r2._2)/(r1._2 + r2._2),(r1._2 + r2._2)))
                     .map{case(a,(b,c)) => (a,b)}
     rd.foreach(println)
-
-
-
-/*    val genereWithMovies = moviesDatasetRDD
-                          .map(movie => (movie.substring(movie.lastIndexOf(',')+1,movie.length).split('|')(0),Array(movie.substring(0,movie.indexOf(',')))))
-                          .reduceByKey(_++_).collect().toVector*/
-
-    //var arr = genereWithMovies.toArray //ok i dati sono (genere, [f1,...
-
-   /* var moviesDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/moviesLight.csv")
-                     //.map(movie => (Array(movie.substring(movie.lastIndexOf(','),movie.length-1).split('|').distinct).toVector,Array(movie.split(",")(0).toVector)))
-                     .map(movie => (movie.substring(movie.lastIndexOf(','),movie.length-1).split('|'),Array(movie.substring(0,movie.indexOf(',')))))
-                     .reduceByKey(_++_)
-                     .collect()
-                     .foreach(println)*/
-
-//    var ratingsDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/ratings.csv")
-//                      .flatMap(movie => movie.split(",").takeRight(1)(0).split('|'))
-//                      .map((_,1))
-//                      .reduceByKey(_+_)
-//                      .collect.toMap
-//                      .foreach(println)
 
   }
 
