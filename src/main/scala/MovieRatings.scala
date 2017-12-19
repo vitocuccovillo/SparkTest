5526e775e06f8c36101bf6ec89a71bc291c1b037
vitocuccovillo <vito.cuccovillo@gmail.com>
2017-12-19 16:16:46 +0100

    exercise completed!

--- src/main/scala/MovieRatings.scala
+++ src/main/scala/MovieRatings.scala
@@ -19,29 +19,7 @@ object MovieRatings {
 
     val rd = merged.flatMap(x => x._2._1.split('|').map(y => (y,(x._2._2.toDouble,1)))).reduceByKey{case ((r1,c1),(r2,c2)) => (r1 + r2,c1 + c2)}
     val result = rd.map{ case (gen,(sum,count)) => (gen, sum / count)}
-    rd.foreach(println)
     result.foreach(println)
-
-
-/*    val genereWithMovies = moviesDatasetRDD
-                          .map(movie => (movie.substring(movie.lastIndexOf(',')+1,movie.length).split('|')(0),Array(movie.substring(0,movie.indexOf(',')))))
-                          .reduceByKey(_++_).collect().toVector*/
-
-    //var arr = genereWithMovies.toArray //ok i dati sono (genere, [f1,...
-
-   /* var moviesDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/moviesLight.csv")
-                     //.map(movie => (Array(movie.substring(movie.lastIndexOf(','),movie.length-1).split('|').distinct).toVector,Array(movie.split(",")(0).toVector)))
-                     .map(movie => (movie.substring(movie.lastIndexOf(','),movie.length-1).split('|'),Array(movie.substring(0,movie.indexOf(',')))))
-                     .reduceByKey(_++_)
-                     .collect()
-                     .foreach(println)*/
-
-//    var ratingsDB = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/ratings.csv")
-//                      .flatMap(movie => movie.split(",").takeRight(1)(0).split('|'))
-//                      .map((_,1))
-//                      .reduceByKey(_+_)
-//                      .collect.toMap
-//                      .foreach(println)
 
   }
 
--- src/main/scala/MovieRatingsNew.scala
+++ src/main/scala/MovieRatingsNew.scala
@@ -1,7 +1,7 @@
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-object MovieRatingsOptimized {
+object MovieRatingsNew {
 
   def main(args: Array[String]): Unit = {
 
@@ -13,15 +13,16 @@ object MovieRatingsOptimized {
 
     //coppie (FILMID,GENERI(con pipe))
     val moviesWithGen:RDD[(String,String)] = moviesDatasetRDD.map(movie => (movie.substring(0,movie.indexOf(',')),movie.substring(movie.lastIndexOf(',')+1,movie.length)))
+    //coppie MOVIEID,Genere (tenendo il map all'interno, mappo ogni pezzo dello split con il suo movieid)
+    val mg = moviesWithGen.flatMap(x => x._2.split('|').map(y=> (x._1,y)))
     //coppie (FILMID,VOTO)
     val ratings:RDD[(String,String)] = ratingsDatasetRDD.map(rating => (rating.split(",")(1),rating.split(",")(2)))
     //RDD con coppie (FILMID, (listageneri, voto))
-    val merged:RDD[(String,(String,String))] = moviesWithGen.join(ratings)
-    val rd:RDD[(String,Double)] = merged.flatMap(x => x._2._1.split('|')
-                                        .map(y => (y,(x._2._2.toDouble,1))))
-                                        .reduceByKey((r1,r2) => ((r1._1*r1._2 + r2._1*r2._2)/(r1._2 + r2._2),(r1._2 + r2._2)))
-                                        .map{case(a,(b,_)) => (a,b)}
-    rd.foreach(println)
+    val merged:RDD[(String,(String,String))] = mg.join(ratings)
+
+    val genAvg = merged.map({case(_,(gen,rat)) => (gen, (rat.toDouble,1))})
+                       .reduceByKey({case (r1,r2) => ((r1._1*r1._2 + r2._1*r2._2)/(r1._2 + r2._2),(r1._2 + r2._2))})
+    var result = genAvg.foreach(println)
 
   }
 
