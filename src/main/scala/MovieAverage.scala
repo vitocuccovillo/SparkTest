067308f24c1e35e1794d0fe9a2c12ec604c0a246
vitocuccovillo <vito.cuccovillo@gmail.com>
2017-12-19 11:19:57 +0100

    exercise completed!

---
+++ src/main/scala/MovieAverage.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object MovieAverage {
+
+  def main(args : Array[String]) : Unit = {
+
+    val conf = new SparkConf().setMaster("local[*]").setAppName("MovieAverage")
+    val sc = new SparkContext(conf)
+
+    val moviesDatasetRDD = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/movies.csv")
+    val ratingsDatasetRDD = sc.textFile("file:///C:/Users/vitoc/Desktop/Materiale Tesi/Esercitazione/ratings.csv")
+
+    val movieRDD:RDD[(String,String)] = moviesDatasetRDD.map(row => (row.split(",")(0), row.split(",")(1)))
+    //movieRDD.foreach(println)
+
+    val ratingsRDD:RDD[(String,Double)] = ratingsDatasetRDD.map(row => (row.split(",")(1), row.split(",")(2).toDouble))
+    //ratingsRDD.foreach(println)
+
+    val ds = movieRDD.join(ratingsRDD).map{case (_,(m,r)) => (m,(r,1))}.reduceByKey((a,b) => (a._1+b._1,a._2 + b._2))
+    ds.foreach(println)
+
+  }
+
+}
