0419c56c2e8283c0d137d44396089abd7cc044b7
vitocuccovillo <vito.cuccovillo@gmail.com>
2018-01-04 19:47:48 +0100

    iris exercise

--- src/main/scala/IrisML.scala
+++ src/main/scala/IrisML.scala
@@ -1,14 +1,17 @@
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.ml.feature.StringIndexer
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.{Row, SparkSession}
 
 object IrisML {
 
+  // FROM: https://github.com/MingChen0919/learning-apache-spark/blob/master/naive-bayes-classification.ipynb
+
   def main(args:Array[String]) : Unit = {
 
     val spark = SparkSession.builder.master("local[*]").appName("Bank").getOrCreate()
     val _iris = spark.read.option("header",true).option("inferSchema",true).csv("data/iris.csv")
-    //_iris.printSchema()
-    _iris.show(5)
 
     import spark.implicits._
 
@@ -17,6 +20,23 @@ object IrisML {
                             }.toDF("features","species")
     iris.show()
 
+    val stringIndexer = new StringIndexer().setInputCol("species").setOutputCol("label")
+    val stages = Array(stringIndexer)
+    val pipeline = new Pipeline().setStages(stages)
+
+    val iris_df = pipeline.fit(iris).transform(iris)
+    iris_df.show(5)
+    iris_df.describe().show(5)
+
+    val Array(train, test) = iris_df.randomSplit(Array(0.8, 0.2), seed=1234)
+
+    val model = new NaiveBayes().setModelType("multinomial").setSmoothing(1.0).fit(train)
+    val prediction = model.transform(test)
+    prediction.show()
+
+    val acc = 1.0*prediction.filter(x => x.getAs[Double](2) == x.getAs[Double](5)).count() / test.count()
+    println("Accuratezza: " + acc)
+    model.write.overwrite.save("data/iris_model")
 
   }
 
